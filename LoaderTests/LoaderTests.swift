d72994ee3c985653342fb609f72d7afdaf70d772
Manu Singh <manukumar9334994270@gmail.com>
2019-04-08 23:39:28 +0530

    Initial Commit

---
+++ LoaderTests/LoaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  LoaderTests.swift
+//  LoaderTests
+//
+//  Created by Manu Singh on 08/04/19.
+//  Copyright © 2019 Manu Singh. All rights reserved.
+//
+
+import XCTest
+@testable import Loader
+
+class LoaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
